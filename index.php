47cba8c6dcd76f82fcdd220911c2f3963431cc05
agenciageek <agenciageek@live.com>
2014-12-06 16:25:17 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+    <?php
+    include("Clogin.php");
+
+    $objeto=new Login;
+
+    $objeto->abre("TELA DE CADASTRO");
+
+    $objeto->f_index();
+     //O ARQUIVO É INDEX.PHP E NÃO F_INDEX.PHP.....
+    $objeto->fecha();
+     
+
+     ?>
