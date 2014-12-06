d582cfa92a7bf1a2982add2df9644b2b436a57a0
agenciageek <agenciageek@live.com>
2014-12-06 16:09:47 -0300

    Create telacadastrogeral.php

---
+++ telacadastrogeral.php
@@ -0,0 +1,15 @@
+<?php
+	class TelaCadastro extends  Tela{
+		function TelaCadastroGeral ($nome,$endereco,$telefone,$celular,$cpf,$usuario,$senha,$acao){
+          echo "
+          <form action='$acao' method='post'>
+          table width
+
+                    "
+                  
+		}
+
+
+	}
+
+?>
